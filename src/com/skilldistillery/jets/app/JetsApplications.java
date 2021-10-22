4be8de363444a1edb19241131df67b3f545016df
Chris Lee <91095529+Clee350z@users.noreply.github.com>
2021-10-22 17:12:53 -0600

    inital commit

---
+++ src/com/skilldistillery/jets/app/JetsApplications.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entities.Airfield;
+
+public class JetsApplications {
+
+	private Airfield airField;
+	public static void main(String[] args) {
+
+		
+	}
+
+}
